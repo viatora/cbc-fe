a5d80f282f019e4fa311f8ab22c5660344b72a01
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-31 13:01:10 +0000

    feat: install parallax

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,27 @@
 import jumpCropped from "../assets/images/cbc-jump-cropped.jpg";
 import blackLogo from "../assets/images/cbc-black-logo.png";
+import bandWindow from "../assets/images/cbc-band-window-pic.jpg";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 export default function Home() {
   return (
     <main>
-      <div className="wrapper relative">
-        <img src={jumpCropped} alt="" />
-        <img
-          className="absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-2xl "
-          src={blackLogo}
-          alt=""
-        />
-      </div>
+      <Parallax pages={2} style={{ top: "0", left: "0" }}>
+        <ParallaxLayer offset={0.2} speed={2.5}>
+          <img src={jumpCropped} alt="" />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.9} speed={5}>
+          <img className="max-w-2xl m-auto top-3/4" src={blackLogo} alt="" />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.95} speed={2}>
+          <img src={bandWindow}></img>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.99} speed={1}>
+          <h1 className="text-9xl text-center text-[#febc12]">
+            Welcome to our website
+          </h1>
+        </ParallaxLayer>
+      </Parallax>
     </main>
   );
 }
