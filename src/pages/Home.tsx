9e320ba1caf179d11be93e82128df22098f892cb
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-31 12:37:30 +0000

    feat: create rudimentary homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,17 @@
+import jumpCropped from "../assets/images/cbc-jump-cropped.jpg";
+import blackLogo from "../assets/images/cbc-black-logo.png";
+
 export default function Home() {
-    return (
-      <main>
-        <h2>Home</h2>
-        <p>This is the Home section.</p>
-      </main>
-    );
-};
+  return (
+    <main>
+      <div className="wrapper relative">
+        <img src={jumpCropped} alt="" />
+        <img
+          className="absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-2xl "
+          src={blackLogo}
+          alt=""
+        />
+      </div>
+    </main>
+  );
+}
