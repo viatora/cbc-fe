0d4d75cfad4d377bf763c548297680bfa8d8865c
maxitect <maxime.downe@gmail.com>
2024-11-17 23:24:33 +0000

    chore: update navbar to be transparent and responsive layered on top of video

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,19 +1,57 @@
-// const navItemClasses = "hover:text-gray-400";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const menuLinks = [
+  { linkAddress: "/", linkText: "Home" },
+  { linkAddress: "/about", linkText: "About" },
+  { linkAddress: "/news", linkText: "News" },
+  { linkAddress: "/tour", linkText: "Tour" },
+  { linkAddress: "/music", linkText: "Music" },
+  { linkAddress: "/store", linkText: "Store" },
+  { linkAddress: "/contact", linkText: "Contact" },
+];
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav>
-      {/* <ul className="flex space-x-4 list-none">
-                <li>
-                    <a href="/" className={navItemClasses}>Home</a>
-                </li>
-                <li>
-                    <a href="/about" className={navItemClasses}>About</a>
-                </li>
-                <li>
-                    <a href="/contact" className={navItemClasses}>Contact</a>
-                </li>
-            </ul> */}
+    <nav className="relative">
+      {/* Hamburger button (visible on small screens) */}
+      <div className="md:hidden">
+        <button
+          onClick={toggleMenu}
+          className="text-white focus:outline-none"
+          aria-label="Toggle Menu"
+        >
+          {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </button>
+      </div>
+      {/* Menu items */}
+      <ul
+        className={`${
+          isMenuOpen ? "block" : "hidden"
+        } absolute top-full right-0 w-full bg-transparent md:justify-center md:flex md:gap-[10vw] list-none md:static text-center`}
+      >
+        {menuLinks.map((menuLink) => (
+          <li
+            key={menuLink.linkAddress}
+            className="p-2 md:p-0 text-6xl md:text-2xl"
+          >
+            <Link
+              to={menuLink.linkAddress}
+              className="hover:text-[#FEBC12] font-[Blanch-Caps] text-center"
+              onClick={() => setIsMenuOpen(false)} // Close menu on link click (for mobile)
+            >
+              {menuLink.linkText}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
