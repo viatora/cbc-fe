4b96f3b8eba07ebe16b13a0c6e09a3010abf4a3d
maxitect <maxime.downe@gmail.com>
2024-11-30 22:10:57 +0000

    chore: add social media icon links into Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,80 @@
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaSpotify,
+  FaSoundcloud,
+  FaTiktok,
+  FaYoutube,
+  FaAmazon,
+  FaApple,
+} from "react-icons/fa6";
+
+interface SocialLink {
+  icon: (props: { className: string }) => JSX.Element;
+  url: string;
+  label: string;
+}
+
+const socialLinks: SocialLink[] = [
+  {
+    icon: FaFacebookF,
+    url: "https://facebook.com/clarksbowlingclub",
+    label: "Facebook",
+  },
+  {
+    icon: FaInstagram,
+    url: "https://instagram.com/clarksbowlingclub",
+    label: "Instagram",
+  },
+  {
+    icon: FaSpotify,
+    url: "https://open.spotify.com/artist/4K3IjWGpwXYwjyNONZwvMZ",
+    label: "Spotify",
+  },
+  {
+    icon: FaSoundcloud,
+    url: "https://soundcloud.com/clarksbowlingclub",
+    label: "Soundcloud",
+  },
+  {
+    icon: FaTiktok,
+    url: "https://tiktok.com/@clarksbowlingclub",
+    label: "TikTok",
+  },
+  {
+    icon: FaYoutube,
+    url: "https://youtube.com/@clarksbowlingclub",
+    label: "YouTube",
+  },
+  {
+    icon: FaAmazon,
+    url: "https://music.amazon.com/artists/B073M72RKM/clark's-bowling-club",
+    label: "Amazon Music",
+  },
+  {
+    icon: FaApple,
+    url: "https://music.apple.com/us/artist/clarks-bowling-club/1254533543",
+    label: "Apple Music",
+  },
+];
+
 export default function Footer() {
   return (
-    <footer>
-      {/* <p>&copy; 2024 &nabla; Viatora Digital. All rights reserved.</p> */}
+    <footer className="bg-transparent h-32 md:h-12 flex justify-center align-bottom">
+      <div className="flex flex-wrap justify-center gap-4 px-4 mb-4 h-10 align-bottom">
+        {socialLinks.map((link) => (
+          <a
+            key={link.label}
+            href={link.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-clarks-orange transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-clarks-orange focus:ring-offset-2 focus:ring-offset-transparent rounded-full p-1"
+            aria-label={link.label}
+          >
+            <link.icon className="w-10 h-10 md:w-5 md:h-5" />
+          </a>
+        ))}
+      </div>
     </footer>
   );
 }
