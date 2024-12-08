1dcbe2935edee4a27ca6e8ee3c8e80242e200cac
maxitect <maxime.downe@gmail.com>
2024-12-08 22:27:45 +0000

    chore: updates types.ts with tour date type object interface

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,3 +2,11 @@ export interface Page {
   title: string;
   content: string;
 }
+
+export type TourDateType = {
+  eventDate: string;
+  location: string;
+  venue: string;
+  locationUrl?: string;
+  ticketUrl?: string;
+};
